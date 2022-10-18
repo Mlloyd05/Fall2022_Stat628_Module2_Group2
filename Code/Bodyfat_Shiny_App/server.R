010eec81ef9303504b2377f79a502477d354423c
Marwan Lloyd <Marwan.Lloyd@wisc.edu>
2022-10-17 21:22:25 -0500

    Shiny App Updates and Deployment

--- Code/Bodyfat_Shiny_App/server.R
+++ Code/Bodyfat_Shiny_App/server.R
@@ -1,3 +1,5 @@
+body_fat <- read.csv('Shiny_Data/adjusted_bodyfat.csv')
+
 server <- function(input, output) { 
   
   observeEvent(input$Weight,{
@@ -16,11 +18,25 @@ server <- function(input, output) {
     }
     updateNumericInput(getDefaultReactiveDomain(), "Abdomen", value = round(y,digits = 2))})
   
-  output$calculated_bodyfat <- renderText(paste(c(round(-40.47 - .31*input$Weight + .91 * input$Abdomen,2)),"%"))
-  
-  output$general_scatter <-  renderPlot({ 
-    ggplot(body_fat,aes(input$stat_dropdown,BODYFAT)) + 
-      labs(x = input$stat_dropdown, y = "Body Fat", title = "Body Fat Scatterplot") +
-      geom_jitter(aes(x = input$stat_dropdown,y = BODYFAT, color = input$stat_dropdown, size = BODYFAT))
+  output$calculated_bodyfat <- renderText({
+    percentage <- round(-40.47 - .31*input$Weight + .91 * input$Abdomen,2)
+    
+    if ((percentage >= 0) & (percentage <= 100)){paste(c(round(-40.47 - .31*input$Weight + .91 * input$Abdomen,2),"%"))}
+    else if(percentage > 100){paste(">100%")}
+    else if(percentage < 0) {paste("<0%")}
+    
+    })
+  output$dataset_placement <- renderText({
+    place <- 1 - length(body_fat$BODYFAT[body_fat$BODYFAT > round(-40.47 - .31*input$Weight + .91 * input$Abdomen,2)])/ length(body_fat$BODYFAT)
+    
+    if(place < 0){
+      place <- 0
+    }
+    if(place > 1){
+      place <- 1
+    }
+    
+    paste(c(place * 100 ,"%"))
   })
+  
 }
--- Code/Bodyfat_Shiny_App/ui.R
+++ Code/Bodyfat_Shiny_App/ui.R
@@ -2,7 +2,6 @@ library(shiny)
 library(shinythemes)
 library(rsconnect)
 
-
 ui <- 
   navbarPage("Group 2 Shiny", collapsible = TRUE, inverse = TRUE, theme = shinytheme("spacelab"),
              #             tabPanel("All Stat Scatterplots",
@@ -11,14 +10,17 @@ ui <-
              #                      plotOutput("general_scatter")
              #                      )),
              tabPanel("Model Calculator",
-                      tags$p("The model that our project decided to use was a multi-linear regression of body fat percentage on two variables: weight and abdomen circumfrence. The final equation (used in this calculator) determined for this regression was:"), tags$h3("Body Fat % = -40.47 - .31*Weight + .91 * Abdomen"), tags$h4("---------------------------------------------------------"),
-                      sidebarLayout(sidebarPanel(numericInput("Weight", "Weight (in kg):", 0, min = 0, step = .01), numericInput("Abdomen", "Abdomen Circumfrence (in cm):", 0, min = 0, step = .01)),
-                                    mainPanel("Calculated Bodyfat:", textOutput("calculated_bodyfat"), tags$head(tags$style("#calculated_bodyfat{color: black;
-                                 font-size: 20px;
-                                 font-style: bold;
+                      sidebarLayout(sidebarPanel(numericInput("Weight", "Weight (in kg):", 0, min = 0, step = .01), numericInput("Abdomen", "Abdomen Circumference (in cm):", 0, min = 0, step = .01)),
+                                    mainPanel("Calculated Body Fat percentage (Top) and Percentile Relative to the Dataset (Bottom):", verbatimTextOutput("calculated_bodyfat"),verbatimTextOutput("dataset_placement"), tags$head(tags$style("#calculated_bodyfat{color: black;
+                                 font-size: 16px;
                                  }"
                                     )
-                                    )))
+                                    )),
+                      )
              ),
-             tabPanel("About", tags$p("This shiny app is maintained by Marwan Lloyd '23. For any questions, concerns, or feedback please reach out him at either of the below emails:"), tags$address("Marwan.Lloyd@Wisc.edu"), tags$address("Melloyd2@Wisc.edu"))
+             
+             tabPanel("About", p("The model that our project decided to use was a multi-linear regression of body fat percentage on two variables: weight and abdomen circumfrence. The final equation (used in this calculator) determined for this regression was:"), 
+                      em("Body Fat % = -40.47 - .31*Weight + .91 * Abdomen" )),
+             
+             tabPanel("Contact", p("This shiny app is maintained by Marwan Lloyd '23. For any questions, concerns, or feedback please reach out him at either of the below emails:"), tags$address("Marwan.Lloyd@Wisc.edu"), tags$address("Melloyd2@Wisc.edu"))
   )
